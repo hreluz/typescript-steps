2f5fbdc6d07fbd0bc9a65438cdec07a717d623af
Hector <hmreluz@gmail.com>
2023-06-13 20:58:00 -0500

    01-bases: numbers

---
+++ 01-bases/types/02-numbers.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let avengers = 15;
+    console.log(avengers);
+    const villans = 20;
+    if (avengers > villans) {
+        console.log('We have a problem');
+    }
+    else {
+        console.log('We are saved');
+    }
+    avengers = Number('55 A');
+    console.log({ avengers });
+    // NaN is considered also a Number
+})();
