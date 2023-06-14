de3ce9d2adb1efc77a597afb08350dd26d42fe1b
Hector <hmreluz@gmail.com>
2023-06-14 17:08:28 -0500

    01-bases: arrays

---
+++ 01-bases/types/05-arrays.ts
@@ -0,0 +1,16 @@
+(() => {
+    // const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8];
+    
+    // numbers.push(true);
+
+    // numbers.push(11);
+
+    const numbers: number[] = [1,2,3,4,5,6];
+
+    console.log(numbers);
+
+    const villains = ['Venom', 'Carnage', 'Green Globin'];
+
+    villains.forEach(v => console.log(v.toUpperCase()));
+
+})();
