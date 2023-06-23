7ea039822f623da23d1b63d28e170a91a87420bb
Hector <hmreluz@gmail.com>
2023-06-23 15:03:08 -0500

    02-functions: params rest

---
+++ 01-bases/functions/05-args-rest.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const fullName = (firstName, ...restArgs) => {
+        return `${firstName} ${restArgs.join(' ')}`;
+    };
+    const superman = fullName('Clark', 'Joseph', 'Kent', 'a.k.a Kal-EL');
+    console.log({ superman });
+})();
