36a248983ce0e36ea50c340bed43b9cb60662aee
Hector <hmreluz@gmail.com>
2023-07-06 20:44:48 -0500

    05-ES6: destruct

---
+++ 01-bases/ES6/destruct.ts
@@ -0,0 +1,29 @@
+(() => {
+
+    type Avengers = {
+        nick: string;
+        ironman: string;
+        vision: string;
+        active: boolean;
+        power: number;
+    }
+
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr',
+        vision: 'Paul Bettany',
+        active: true,
+        power: 1500
+    }
+
+    const {power, vision} = avengers;
+
+    console.log(power.toFixed(2), vision.toUpperCase())
+
+    const printAvenger = (avengers:Avengers) => {
+        console.log(avengers.vision)
+    }
+
+    printAvenger(avengers)
+
+})()
--- 01-bases/main.js
+++ 01-bases/main.js
@@ -1,5 +1,20 @@
 "use strict";
 (() => {
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr',
+        vision: 'Paul Bettany',
+        active: true,
+        power: 1500
+    };
+    const { power, vision } = avengers;
+    console.log(power.toFixed(2), vision.toUpperCase());
+    const printAvenger = (avengers) => {
+        console.log(avengers.vision);
+    };
+    printAvenger(avengers);
+})();
+(() => {
     const NOMBRE = 'Hector';
     const getName = function () { };
 })();
