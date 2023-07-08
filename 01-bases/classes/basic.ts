61544ded6e4bb6e639ba7d02ad8d20aca0e8eb9d
Hector <hmreluz@gmail.com>
2023-07-07 22:16:17 -0500

    06-classes: public and private methods

--- 01-bases/classes/basic.ts
+++ 01-bases/classes/basic.ts
@@ -5,6 +5,9 @@
         // public team: string
         // realName?:string; // is public by default
         static avgAge: number = 35;
+        static getAvgAge() {
+            return this.name;
+        }
 
         constructor(private name:string, public team:string, public realName?:string) {
             // this.name = name;
@@ -12,11 +15,17 @@
             // this.realName = realName;
         }
 
+        bio() {
+            return `${this.name} (${this.team})`
+        }
+
     }
 
     const antman: Avenger = new Avenger('Antman', 'Cap America', 'Scott Lang');
     console.log(antman.realName);
     console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getav());
 
 
 })();
--- 01-bases/main.js
+++ 01-bases/main.js
@@ -1,15 +1,23 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
         }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Cap America', 'Scott Lang');
     console.log(antman.realName);
     console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getav());
 })();
 //# sourceMappingURL=main.js.map
