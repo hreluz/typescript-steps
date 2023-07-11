764c485e3899cd6e1b1db1d677fc65cf4f341ce4
Hector <hmreluz@gmail.com>
2023-07-11 18:40:26 -0500

    09-generics: factory decorators

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,8 +2,15 @@ function printToConsole(constructor:Function) {
     console.log(constructor);
 }
 
+const printToConsoleConditional = (print:boolean = false):Function => {
+    if (print) {
+        return printToConsole
+    }
+    return () => console.log('Hello world')
+}
+
 // It executes only in the transpilation
-@printToConsole
+@printToConsoleConditional(false)
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co';
