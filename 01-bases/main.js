8ee4683b9fe30a938c45fb807898c20466a3fe59
Hector <hmreluz@gmail.com>
2023-07-11 13:53:39 -0500

    08-namespace: creating a namespace

--- 01-bases/main.js
+++ 01-bases/main.js
@@ -1,75 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super speed', 'time traveller']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length > 3;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super force'],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? false : true;
     };
-    console.log(superman.name);
-})();
-(() => {
-    class Mutant {
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Hector',
-        address: {
-            id: 4,
-            zip: '84453',
-            city: 'Seattle'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    console.log(client.getFullAddress(''));
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelear();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelear() {
-        console.log("...... gogogo!!!");
-    }
-};
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    imprimirBio() {
-        console.log(this.nombre + ' ' + this.estadoCivil);
-    }
-}
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('hec'));
 //# sourceMappingURL=main.js.map
