b9da7bef02f32912041390fd66759fd9cf2fa14f
Hector <hmreluz@gmail.com>
2023-07-11 15:48:24 -0500

    09-generics: generic function example

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,7 +2,11 @@
 // import * as HeroClasses from "./classes/hero";
 // import powers2 from "./data/powers";
 
-import { printObject, genericFunction, genericFunctionArrow } from "./generics/generic";
+import { genericFunctionArrow } from "./generics/generic"
+import { Hero } from "./interfaces/hero"
+import { Villain } from "./interfaces/villain"
+
+// import { printObject, genericFunction, genericFunctionArrow } from "./generics/generic";
 
 
 // const Hero = 123;
@@ -17,13 +21,21 @@ import { printObject, genericFunction, genericFunctionArrow } from "./generics/g
 // console.log(ironman.power)
 
 
-printObject(123);
-printObject(new Date());
-printObject({a:1 ,b:2, c:3});
-printObject([1,2,3,4,5,6,7,8])
-printObject('Hola Mundo')
+// printObject(123);
+// printObject(new Date());
+// printObject({a:1 ,b:2, c:3});
+// printObject([1,2,3,4,5,6,7,8])
+// printObject('Hola Mundo')
 
-console.log('Generic function')
-console.log(genericFunction(3.141516).toFixed(2))
-console.log(genericFunction('Hello World').toUpperCase())
-console.log(genericFunctionArrow(new Date()).getDate())
+// console.log('Generic function')
+// console.log(genericFunction(3.141516).toFixed(2))
+// console.log(genericFunction('Hello World').toUpperCase())
+// console.log(genericFunctionArrow(new Date()).getDate())
+
+const deadPool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130
+}
+
+console.log(genericFunctionArrow<Villain>(deadPool).dangerLevel)
